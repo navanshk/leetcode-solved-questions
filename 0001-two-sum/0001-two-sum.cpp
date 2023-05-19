a200cff516d391165257eee6b1694e05c08e2f4c
navanshk <97936802+navanshk@users.noreply.github.com>
2023-05-19 15:34:57 +0530

    Time: 27 ms (49.98%), Space: 12.5 MB (5.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,17 +3,19 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
         vector<int>ans;
+        map<int,int>mp;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
-            {
-                if((nums[i]+nums[j])==target)
+            int a=nums[i];
+            int more=target-a;
+            
+                if(mp.find(more)!=mp.end())
                 {
+                    ans.push_back(mp[more]);
                     ans.push_back(i);
-                    ans.push_back(j);
                 }
                     
-            }
+            mp[a]=i;
             
         }
         return ans;
