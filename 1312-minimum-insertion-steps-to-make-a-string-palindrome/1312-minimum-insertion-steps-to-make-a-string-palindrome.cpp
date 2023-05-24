fe2fa9719a8345a0d8073d8663658756ee3458d5
navanshk <97936802+navanshk@users.noreply.github.com>
2023-05-24 07:56:20 +0530

    Time: 55 ms (62.93%), Space: 10.8 MB (62.92%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     int longestCommonSubsequence(string &a, string &b) {
+    int m[1001][1001] = {};
+    for (auto i = 0; i < a.size(); ++i)
+        for (auto j = 0; j < b.size(); ++j)
+            m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
+    return m[a.size()][b.size()];
+
+    }
+    int minInsertions(string s) {
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+            int l=longestCommonSubsequence(s,s1);
+        int n=s.length();
+        return n-l;
+    }
+};
