961057773ec494a8857bbbeefd28a3e898ea1645
navanshk <97936802+navanshk@users.noreply.github.com>
2023-06-08 09:11:56 +0530

    Time: 26 ms (27.82%), Space: 16.7 MB (82.19%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void SetNext(Node *current, Node *left)
+    {
+        while(current != nullptr)
+        {
+            left->next = current->right;
+            left = left->next;
+            current = current->next;
+
+            if (current != nullptr)
+            {
+                left->next = current->left;
+                left = left->next;
+            }
+        }
+    }
+    
+    void connectRecursive(Node *current)
+    {
+        if (current->left == nullptr)
+        {
+            return;
+        }
+
+        SetNext(current, current->left);
+
+       connectRecursive(current->left);
+
+    }
+    
+    Node* connect(Node* root) {
+        if (root == nullptr)
+        {
+            return root;;
+        }
+       connectRecursive(root);
+        return root;
+        
+    }
+};
