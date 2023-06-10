496038602e8301487e5faaeddc4b3905c6e3e077
navanshk <97936802+navanshk@users.noreply.github.com>
2023-06-10 16:54:30 +0530

    Time: 27 ms (41.21%), Space: 14 MB (42.34%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            for (int it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> pq;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                pq.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        while (!pq.empty()) {
+            int curr = pq.front();
+            topo.push_back(curr);
+            pq.pop();
+            
+            for (int it : adj[curr]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    pq.push(it);
+                }
+            }
+        }
+        
+        if (topo.size() != numCourses) {
+            return {}; // Return empty array if there is a cycle
+        }
+        
+        return topo; 
+    }
+};
