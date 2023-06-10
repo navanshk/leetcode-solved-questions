c0e8394d1983f55dbce618395f279cdfad88d0cf
navanshk <97936802+navanshk@users.noreply.github.com>
2023-06-10 19:56:17 +0530

    Time: 226 ms (34.31%), Space: 60.8 MB (28.33%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool dfs(int node,vector<int> &path,vector<int> &v,vector<int> &check,vector<int> adj[])
+  {
+      v[node]=1;
+      path[node]=1;
+      for(auto i:adj[node])
+      {
+          if(v[i]==0 && path[i]==0)
+          {
+              if(dfs(i,path,v,check,adj)==true)
+              {
+                  return true;
+              }
+          }
+          else if(v[i]==1 && path[i]==1)
+              {
+                  return true;
+              }
+      }
+      path[node]=0;
+      check[node]=1;
+      return false;
+  }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // first of all creating a adjlist
+        int V=graph.size(); // total number of rows present in graph
+        vector<int> adj[V];
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+        // adj[] has been created..
+        // some observations
+        // 1.any node part of a cycle cant be a safe node
+        // 2.any node leading to cycle node also cant be a safe node
+
+        // finding cycle nodes
+        // traversing through each component of graph
+
+       vector<int> v(V,0);
+        vector<int> path(V,0);
+        vector<int> check(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(v[i]==0)
+            {
+                dfs(i,path,v,check,adj);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
